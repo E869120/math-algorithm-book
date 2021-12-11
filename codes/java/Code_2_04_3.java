b87167a4c76700a53d689a5dbd477d63552827d8
Hirotaka Yoneda <squar37@gmail.com>
2021-12-11 22:24:25 +0900

    Update Code_2_04_3.java and Code_2_06_1.java

--- codes/java/Code_2_04_3.java
+++ codes/java/Code_2_04_3.java
@@ -2,9 +2,12 @@ import java.util.*;
 
 class Code_2_04_3 {
 	public static void main(String[] args) {
+		// 入力
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int S = sc.nextInt();
+		
+		// 答えを求める
 		int cnt = 0;
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
@@ -13,6 +16,8 @@ class Code_2_04_3 {
 				}
 			}
 		}
+		
+		// 出力
 		System.out.println(cnt);
 	}
 }
--- codes/java/Code_2_06_1.java
+++ codes/java/Code_2_06_1.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Code_2_06_1 {
 	public static void main(String[] args) {
+		// 入力
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int S = sc.nextInt();
@@ -9,12 +10,16 @@ class Code_2_06_1 {
 		for (int i = 1; i <= N; i++) {
 			A[i] = sc.nextInt();
 		}
+		
+		// 全パターンを探索：(1L << N) は 2 の N 乗（ただし 1L は long 型の 1 を表す）
 		String answer = "No";
 		for (long i = 0; i < (1L << N); i++) {
 			int sum = 0;
-			for (int j = 0; j < N; j++) {
-				if ((i & (1L << j)) != 0L) {
-					sum += A[j + 1];
+			for (int j = 1; j <= N; j++) {
+				// (i & (1L << (j - 1))) != 0LL の場合、i の 2 進法表記の下から j 桁目が 1
+				// (1L << (j - 1)) は Java では「2 の j-1 乗」を意味します
+				if ((i & (1L << (j - 1))) != 0L) {
+					sum += A[j];
 				}
 			}
 			if (sum == S) {
@@ -22,6 +27,8 @@ class Code_2_06_1 {
 				break;
 			}
 		}
+		
+		// 出力
 		System.out.println(answer);
 	}
 }
