2b716231c689a1c6762957a4797a204f6954d87a
Hirotaka Yoneda <squar37@gmail.com>
2021-12-11 21:33:14 +0900

    Update Code_2_04_2.java

--- codes/java/Code_2_04_2.java
+++ codes/java/Code_2_04_2.java
@@ -2,16 +2,22 @@ import java.util.*;
 
 class Code_2_04_2 {
 	public static void main(String[] args) {
+		// 入力
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int X = sc.nextInt();
 		int Y = sc.nextInt();
+		
+		// 答えを求める
 		int cnt = 0;
 		for (int i = 1; i <= N; i++) {
+			// mod の計算は 2.2 節参照
 			if (i % X == 0 || i % Y == 0) {
 				cnt++;
 			}
 		}
+		
+		// 出力
 		System.out.println(cnt);
 	}
 }
