0596174b4a32f6a8bffb0dbd429d64ded7face08
Hirotaka Yoneda <squar37@gmail.com>
2021-12-16 00:38:52 +0900

    Update Code_2_01_1.java

--- codes/java/Code_2_01_1.java
+++ codes/java/Code_2_01_1.java
@@ -4,6 +4,6 @@ class Code_2_01_1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt(); // 入力部分
-		System.out.println(20 + N); // 出力部分
+		System.out.println(5 + N); // 出力部分
 	}
 };
