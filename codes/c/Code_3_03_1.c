163fb15cc2e3740559e694bec3b903a41aa6cea9
Hirotaka Yoneda <squar37@gmail.com>
2021-12-25 18:23:22 +0900

    Fix Code_3_03_1.c

--- codes/c/Code_3_03_1.c
+++ codes/c/Code_3_03_1.c
@@ -10,11 +10,12 @@ int main() {
 	
 	// 5 つのカードの番号 (i, j, k, l, m) を全探索
 	int answer = 0;
-	for (int i = 1; i <= N; i++) {
-		for (int j = i + 1; j <= N; j++) {
-			for (int k = j + 1; k <= N; k++) {
-				for (int l = k + 1; l <= N; l++) {
-					for (int m = l + 1; m <= N; m++) {
+	int i, j, k, l, m;
+	for (i = 1; i <= N; i++) {
+		for (j = i + 1; j <= N; j++) {
+			for (k = j + 1; k <= N; k++) {
+				for (l = k + 1; l <= N; l++) {
+					for (m = l + 1; m <= N; m++) {
 						if (A[i] + A[j] + A[k] + A[l] + A[m] == 1000) {
 							answer += 1;
 						}
