468c390e81cd2db5f0d3c581969f3ef0a03fa44b
Hirotaka Yoneda <squar37@gmail.com>
2021-12-29 21:34:43 +0900

    Update several C source codes on chapter 4.5

--- codes/c/Code_4_05_2_stack.c
+++ codes/c/Code_4_05_2_stack.c
@@ -1,3 +1,7 @@
+// このソースコードは、深さ優先探索 (DFS) をスタックを用いて実装したものです。
+// スタックは「一番上に要素を積む」「一番上の要素を調べる」「一番上に積まれた要素を取り除く」という 3 種類の操作ができるデータ構造です。
+// 深さ優先探索の部分は、コード 4.5.3 の queue を stack に変更したものをベースに書かれています。
+
 // 隣接リストの作り方はコード 4.5.1 のコメントを参照
 
 #include <stdio.h>
@@ -7,18 +11,7 @@
 int N, M, A[100009], B[100009];
 int degree[100009], cnt[100009];
 int* G[100009];
-
 bool visited[100009];
-
-void dfs(int pos) {
-	visited[pos] = true;
-	int i;
-	for (i = 0; i < degree[pos]; i++) {
-		if (visited[G[pos][i]] == false) {
-			dfs(G[pos][i]);
-		}
-	}
-}
 
 int main() {
 	// 入力
@@ -49,11 +42,30 @@ int main() {
 		cnt[B[i]] += 1;
 	}
 	
-	// 深さ優先探索
+	
+	// スタック S の定義
+	// スタックの中身が S[0], S[1], ..., S[SZ - 1] になるようにする
+	int S[100009], SZ = 0;
+	
+	// 深さ優先探索の初期化
 	for (i = 1; i <= N; i++) {
 		visited[i] = false;
 	}
-	dfs(1);
+	visited[1] = true;
+	S[SZ] = 1; SZ++; // S に 1 を追加
+	
+	// 幅優先探索
+	while (SZ >= 1) {
+		int pos = S[SZ - 1]; // S の先頭を調べる
+		SZ--; // S の先頭を取り出す
+		for (i = 0; i < degree[pos]; i++) {
+			int nex = G[pos][i];
+			if (visited[nex] == false) {
+				visited[nex] = true;
+				S[SZ] = nex; SZ++; // S に nex を追加
+			}
+		}
+	}
 	
 	// 連結かどうかの判定（Answer=true のとき連結）
 	bool answer = true;
