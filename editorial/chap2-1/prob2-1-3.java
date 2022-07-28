385b526fcd0e49acff8d4e5fc2616c5db1e3d189
Masataka Yoneda <e869120@gmail.com>
2022-07-28 11:23:16 +0900

    Create prob2-1-3.java

---
+++ editorial/chap2-1/prob2-1-3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 入力
+		int A1 = sc.nextInt();
+		int A2 = sc.nextInt();
+		int A3 = sc.nextInt();
+
+		// 出力
+		System.out.println(A1 * A2 * A3);
+	}
+};
